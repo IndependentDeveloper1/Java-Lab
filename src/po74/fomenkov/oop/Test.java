1b2ae460b4f3579a8c20e97720e5a00984fb4efd
IndependentDeveloper1 <40405436+IndependentDeveloper1@users.noreply.github.com>
2019-05-16 16:43:47 +0400

    Test

--- src/po74/fomenkov/oop/Test.java
+++ src/po74/fomenkov/oop/Test.java
@@ -10,288 +10,27 @@ public class Test {
 
 
     public static void lab2test() {
-        DebitAccount account1 = new DebitAccount() ;
-        DebitAccount account2 = new DebitAccount() ;
-        DebitAccount account3 = new DebitAccount() ;
+        Account account1 = new DebitAccount() ;
+        Account account2 = new DebitAccount() ;
+        Account account3 = new DebitAccount() ;
 
-        Account account = new Account() {
-            @Override
-            public String getNumber() {
-                return null;
-            }
-
-            @Override
-            public void setNumber(String newNumber) {
-
-            }
-
-            @Override
-            public double getBalance() {
-                return 0;
-            }
-
-            @Override
-            public void setBalance(double newBalance) {
-
-            }
-        };
-        account.setNumber("12");
-        System.out.println(account1.getNumber());
+        Account account = new DebitAccount();
 
 
 
-        /*account1.setNumber("a");
+
+        account1.setNumber("a");
         account1.setBalance(12.5);
         account2.setNumber("b");
         account2.setBalance(59.4);
         account3.setNumber("c");
         account3.setBalance(8.1);
-*/
+
         System.out.println(account1.getNumber());
 
-        Client client1 = new Client() {
-            @Override
-            public boolean add(Account account) {
-                return false;
-            }
-
-            @Override
-            public boolean add(int index, Account account) {
-                return false;
-            }
-
-            @Override
-            public Account get(int index) {
-                return null;
-            }
-
-            @Override
-            public Account get(String accountNumber) {
-                return null;
-            }
-
-            @Override
-            public boolean hasAccount(String accountNumber) {
-                return false;
-            }
-
-            @Override
-            public Account set(int index, Account account) {
-                return null;
-            }
-
-            @Override
-            public Account remove(int index) {
-                return null;
-            }
-
-            @Override
-            public Account remove(String accountNumber) {
-                return null;
-            }
-
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public Account[] getAccounts() {
-                return new Account[0];
-            }
-
-            @Override
-            public Account[] sortedByBalanceAccounts() {
-                return new Account[0];
-            }
-
-            @Override
-            public double totalBalance() {
-                return 0;
-            }
-
-            @Override
-            public String getName() {
-                return null;
-            }
-
-            @Override
-            public void setName(String name) {
-
-            }
-
-            @Override
-            public int indexOf(String accountNumber) {
-                return 0;
-            }
-
-            @Override
-            public void showDetails() {
-
-            }
-        };
-        Client client2 = new Client() {
-            @Override
-            public boolean add(Account account) {
-                return false;
-            }
-
-            @Override
-            public boolean add(int index, Account account) {
-                return false;
-            }
-
-            @Override
-            public Account get(int index) {
-                return null;
-            }
-
-            @Override
-            public Account get(String accountNumber) {
-                return null;
-            }
-
-            @Override
-            public boolean hasAccount(String accountNumber) {
-                return false;
-            }
-
-            @Override
-            public Account set(int index, Account account) {
-                return null;
-            }
-
-            @Override
-            public Account remove(int index) {
-                return null;
-            }
-
-            @Override
-            public Account remove(String accountNumber) {
-                return null;
-            }
-
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public Account[] getAccounts() {
-                return new Account[0];
-            }
-
-            @Override
-            public Account[] sortedByBalanceAccounts() {
-                return new Account[0];
-            }
-
-            @Override
-            public double totalBalance() {
-                return 0;
-            }
-
-            @Override
-            public String getName() {
-                return null;
-            }
-
-            @Override
-            public void setName(String name) {
-
-            }
-
-            @Override
-            public int indexOf(String accountNumber) {
-                return 0;
-            }
-
-            @Override
-            public void showDetails() {
-
-            }
-        };
-        Client client3 = new Client() {
-            @Override
-            public boolean add(Account account) {
-                return false;
-            }
-
-            @Override
-            public boolean add(int index, Account account) {
-                return false;
-            }
-
-            @Override
-            public Account get(int index) {
-                return null;
-            }
-
-            @Override
-            public Account get(String accountNumber) {
-                return null;
-            }
-
-            @Override
-            public boolean hasAccount(String accountNumber) {
-                return false;
-            }
-
-            @Override
-            public Account set(int index, Account account) {
-                return null;
-            }
-
-            @Override
-            public Account remove(int index) {
-                return null;
-            }
-
-            @Override
-            public Account remove(String accountNumber) {
-                return null;
-            }
-
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public Account[] getAccounts() {
-                return new Account[0];
-            }
-
-            @Override
-            public Account[] sortedByBalanceAccounts() {
-                return new Account[0];
-            }
-
-            @Override
-            public double totalBalance() {
-                return 0;
-            }
-
-            @Override
-            public String getName() {
-                return null;
-            }
-
-            @Override
-            public void setName(String name) {
-
-            }
-
-            @Override
-            public int indexOf(String accountNumber) {
-                return 0;
-            }
-
-            @Override
-            public void showDetails() {
-
-            }
-        };
+        Client client1 = new Entity("1") ;
+        Client client2 = new Entity("2") ;
+        Client client3 = new Entity("3") ;
 
 
         AccountManager accountManager = new AccountManager(1);
@@ -322,6 +61,7 @@ public class Test {
 
 
         accountManager.get(0).showDetails();
+        accountManager.
 
 
 
